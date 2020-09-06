5fa183e916130b51bb92f14cfd6c1f20d8aba73a
Charlie Nguyen <36280021+charliekid@users.noreply.github.com>
2020-09-05 20:30:45 -0700

    Got the ball to gain speed when bounces off of paddle

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,7 +5,15 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     private Rigidbody rb;
-    public float amplify = 10;
+    public float amplify = 20;
+
+
+    GameObject rightPaddle;
+    GameObject leftPaddle;
+
+    public float leftScore = 0;
+    public float rightScore = 0;
+    public const float MAX_SCORE = 2;
 
 
 
@@ -13,38 +21,99 @@ public class Ball : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        //AddForce();
-
+        rightPaddle = GameObject.Find("PaddleRight");
+        leftPaddle = GameObject.Find("PaddleLeft");
+        for(int i = 0; i < 15; i++)
+        {
+            AddForce();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
+        // if the ball is passed 11.15 on the x position score is to the left paddle
+        // if ball is pass -40 on the x position score is  the right paddle
+        if (transform.position.x > 11)
+        {
+            leftScore++;
+            if (leftScore == MAX_SCORE)
+            {
+                Debug.Log("GAME OVER!! LEFT PADDLE WON!!!");
+                Debug.Log("Final score |L:" + leftScore + " R:" + rightScore + "|");
+                RestartGame();  
+            }
+            else
+            {
+                LeftScored();
+            }
 
+        }
+        if (transform.position.x < -12)
+        {
+            rightScore++; 
+            if (rightScore == MAX_SCORE)
+            {
+                Debug.Log("GAME OVER!! RIGHT PADDLE WON!!!");
+                Debug.Log("Final SCORE |L:" + leftScore + " R:" + rightScore + "|");
+                RestartGame();
+            }
+            else
+            {
+                RightScored();
+            }
+
+        }
+    }
+
+    public void LeftScored()
+    {
+        Debug.Log("LEFT PERSON SCORES");
+        rightPaddle.transform.position = new Vector3(10, 1, 0);
+        transform.position = new Vector3(9, 1, 0);
+        Debug.Log("CURRENT SCORE |L:" + leftScore + " R:" + rightScore + "|");
+        ResetForce();
+
+    }
+
+    public void RightScored()
+    {
+        Debug.Log("RIGHT PERSON SCORES");
+        leftPaddle.transform.position = new Vector3(-10, 1, 0);
+        transform.position = new Vector3(-9, 1, 0);
+        Debug.Log("CURRENT SCORE |L:" + leftScore + " R:" + rightScore + "|");
+        ResetForce();
+    }
+    public void RestartGame()
+    {
+        leftScore = 0;
+        rightScore = 0;
+
+        rightPaddle.transform.position = new Vector3(10, 1, 0);
+        leftPaddle.transform.position = new Vector3(-10, 1, 0);
+        transform.position = new Vector3(0, 1, 0);
+        Debug.Log("New Game");
+        Debug.Log("CURRENT SCORE |L:" + leftScore + " R:" + rightScore + "|");
 
     }
     /**
-    * Does when something hits something
+    * Changes color of the paddle when hit
     */
     void OnCollisionEnter(Collision other)
     {
-        Debug.Log("I hit something " + gameObject.name);
-        Debug.Log("I hit a " + other.gameObject.name);
-
-        // if want to the change the color that this is attached other we change
-        // the game object
-        // ball changes color
-        //gameObject.GetComponent<MeshRenderer>().material.color = NewColor();
-        // floor changes color
         other.gameObject.GetComponent<MeshRenderer>().material.color = NewColor();
+        for (int i = 0; i < 15; i++)
+        {
+            AddForce();
+        }
 
     }
     private void FixedUpdate()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        /*if (Input.GetKeyDown(KeyCode.Space))
         {
             AddForce();
-        }
+        }*/
 
     }
 
@@ -56,7 +125,13 @@ public class Ball : MonoBehaviour
     private void ResetForce()
     {
         // goes straight down the x axis
-        rb.AddForce(new Vector3(1, 0, 0));
+        //rb.AddForce(new Vector3(1, 0, 0));
+        rb.isKinematic = true;
+        for (int i = 0; i < 15; i++)
+        {
+            AddForce();
+        }
+        rb.isKinematic = false; 
     }
     /**
     * Returns the color when the object hits something
---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    GameObject rightPaddle;
+    GameObject leftPaddle;
+    GameObject ball;
+
+    public float leftScore = 0;
+    public float rightScore = 0;
+    public const float MAX_SCORE = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rightPaddle = GameObject.Find("PaddleRight");
+        leftPaddle = GameObject.Find("PaddleLeft");
+        ball = GameObject.Find("Ball");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if the ball is passed 11.15 on the x position score is to the left paddle
+        // if ball is pass -40 on the x position score is  the right paddle
+        if (ball.transform.position.x > 12)
+        {
+            Debug.Log("We are passing the right paddle");
+            if (leftScore < MAX_SCORE)
+            {
+                LeftScored();
+            }
+            else
+            {
+                RestartGame();
+            }
+
+        }
+        if (ball.transform.position.x < -12)
+        {
+            if (leftScore < MAX_SCORE)
+            {
+                RightScored();
+            }
+            else
+            {
+                RestartGame();
+            }
+
+        }
+    }
+
+    public void LeftScored()
+    {
+        Debug.Log("LEFT PERSON SCORES");
+        leftScore++;
+        rightPaddle.transform.position = new Vector3(10, 1, 0);
+        ball.transform.position = new Vector3(9, 1, 0);
+        Debug.Log("CURRENT SCORE |L:" + leftScore + " R:" + rightScore + "|");
+
+
+    }
+
+    public void RightScored()
+    {
+        Debug.Log("RIGHT PERSON SCORES");
+        rightScore++;
+        leftPaddle.transform.position = new Vector3(-10, 1, 0);
+        ball.transform.position = new Vector3(-9, 1, 0);
+        Debug.Log("CURRENT SCORE |L:" + leftScore + " R:" + rightScore + "|");
+    }
+    public void RestartGame()
+    {
+        leftScore = 0;
+        rightScore = 0;
+
+        rightPaddle.transform.position = new Vector3(10, 1, 0);
+        leftPaddle.transform.position = new Vector3(-10, 1, 0);
+        ball.transform.position = new Vector3(0, 1, 0);
+
+    }
+}
